e8e874e2bfb6df4497229587c1f8a6c30d2d454b
youyiqin <564151947@qq.com>
2020-10-31 02:19:51 +0800

    fix: avatar addr

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styles from "../styles/avatar.module.sass";
 
+const assertPrefix = "/qinyouyi_blog";
 export default function Avatar({ customStyle = {} }) {
   return (
     <div className={styles.logo} style={customStyle}>
       <Link href="/">
         <a>
-          <img src="/avatar.png" alt="my avatar" />
+          <img src={`${assertPrefix}/avatar.png`} alt="my avatar" />
         </a>
       </Link>
     </div>
