7449b84f88cd82103d14e574b8666265048178b9
youyiqin <564151947@qq.com>
2022-02-11 21:30:43 +0800

    test more

--- articleImageCopy.js
+++ articleImageCopy.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const fs = require("fs");
 
-const articleFilePath = process.argv[2];
+const articleFilePathArr = [...process.argv.slice(2)];
 try {
-  fs.appendFileSync("aaa", process.argv.join("\n"), "utf-8");
+  articleFilePathArr.forEach((pathStr) => {
+    fs.appendFileSync("aaa", pathStr, "utf-8");
+  });
 } catch (error) {
   console.log(error);
   throw new Error(error);
