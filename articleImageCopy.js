9f2ef22ed7ab0584dacdebd11c2f3958fbc57a8c
youyiqin <564151947@qq.com>
2022-02-11 21:21:36 +0800

    test more

--- articleImageCopy.js
+++ articleImageCopy.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const fs = require("fs");
-console.log("get a file");
 
-fs.writeFileSync(path.join(__dirname, "aaaa"), "adasdad", "utf-8");
+fs.writeFileSync(
+  path.join(__dirname, "aaaa"),
+  process.argv.join("-"),
+  "utf-8"
+);
