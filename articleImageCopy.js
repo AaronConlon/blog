f6a41292a04d8274e06d054815e1a5b24469330b
youyiqin <564151947@qq.com>
2022-02-11 23:33:26 +0800

    test more

--- articleImageCopy.js
+++ articleImageCopy.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const fs = require("fs");
+const { exec } = require("child_process");
 
+const logError = (err) => {
+  fs.appendFileSync("error.log", `${err.message}\n`, "utf-8");
+};
 const articleFilePathArr = [...process.argv.slice(2)];
 try {
   articleFilePathArr.forEach((pathStr) => {
@@ -9,9 +13,20 @@ try {
     const targetLines = content
       .split("\n")
       .filter((line) => /^!\[.*?\]\(/.test(line));
+    targetLines.forEach((line) => {
+      const relativePath = line.replace(/^.*\]\(/, "").replace(/\]$/, "");
+      const fileName = relativePath.replace(path.dirname(relativePath), "");
+      // copy file
+      exec(
+        `cp ${relativePath} ${path.join(__dirname, "articleImgs")}`,
+        logError
+      );
+      // replace local relative path ro current repo relative path
+      exec(`sed -i s/${relativePath}/../articleImgs${fileName}`, logError);
+    });
     fs.appendFileSync("aaa", targetLines.join("\n"), "utf-8");
   });
 } catch (error) {
-  console.log(error);
+  logError(`${error}\n`);
   throw new Error(error);
 }
