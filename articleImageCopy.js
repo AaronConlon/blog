b008a927aa2fe36f91c3a9c2f9946cf4992b6519
youyiqin <564151947@qq.com>
2022-02-11 21:27:34 +0800

    test more

--- articleImageCopy.js
+++ articleImageCopy.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const articleFilePath = process.argv[2];
 try {
-  fs.appendFileSync("aaa", process.argv.join("/n"), "utf-8");
+  fs.appendFileSync("aaa", process.argv.join("\n"), "utf-8");
 } catch (error) {
   console.log(error);
   throw new Error(error);
