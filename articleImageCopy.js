9356e203e9c64b8c494d44732dd83b5ef54fd999
youyiqin <564151947@qq.com>
2022-02-12 12:13:38 +0800

    test

--- articleImageCopy.js
+++ articleImageCopy.js
@@ -2,38 +2,48 @@ const path = require("path");
 const fs = require("fs");
 const { exec } = require("child_process");
 
-const logError = (err) => {
-  fs.appendFileSync("error.log", `${err}\n`, "utf-8");
+const logError = (error, stdout, stderr) => {
+  if (error) {
+    fs.appendFileSync("error.log", `error 7: ${error}\n`, "utf-8");
+    return;
+  }
+  if (stderr) {
+    fs.appendFileSync("error.log", `error 11:${stderr}\n`, "utf-8");
+    return;
+  }
+  fs.appendFileSync("error.log", `error 14: ${error || stdout}\n`, "utf-8");
 };
+
 const articleFilePathArr = [...process.argv.slice(2)];
 try {
   articleFilePathArr.forEach((pathStr) => {
     // fs.appendFileSync("aaa", pathStr, "utf-8");
-    const content = fs.readFileSync(path.resolve(pathStr), "utf-8");
+    let content = fs.readFileSync(path.resolve(pathStr), "utf-8");
     const targetLines = content
       .split("\n")
       .filter((line) => /^!\[.*?\]\(/.test(line));
+    const replaceKeywordMapArr = [];
     targetLines.forEach((line) => {
       // test
-      logError(`line is:\n${line}\n`);
+      // logError(`line is:\n${line}\n`);
       const relativePath = line.replace(/^.*\]\(/, "").replace(/\)$/, "");
-      logError(`relative path is: \n${relativePath}\n`);
+      // logError(`relative path is: \n${relativePath}\n`);
       const fileName = relativePath.replace(path.dirname(relativePath), "");
-      logError(`fileName path is: \n${fileName}\n`);
+      // logError(`fileName path is: \n${fileName}\n`);
 
       // copy file
-      exec(
-        `cp -n "${relativePath}" "${path.join(__dirname, "articleImgs")}"`,
-        logError
-      );
+      exec(`cp -n "${relativePath}" "${path.join(__dirname, "articleImgs")}"`);
       // replace local relative path ro current repo relative path
-      exec(
-        `sed -i '' -e 's#${relativePath}#../articleImgs${fileName}#g'`,
-        logError
-      );
+      // exec(`sed 's#${relativePath}#../articleImgs${fileName}#g'`, logError);
+      replaceKeywordMapArr.push([relativePath, `../articcleImgs${fileName}`]);
     });
+    replaceKeywordMapArr.forEach(([source, target]) => {
+      content = content.replace(source, target);
+    });
+    // save content to file
+    fs.writeFileSync(path.resolve(pathStr), content, "utf-8");
   });
 } catch (error) {
   logError(`${error}\n`);
-  throw new Error(error);
+  // throw new Error(error);
 }
