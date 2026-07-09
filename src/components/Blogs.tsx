7eb4b8dd7382f5751ee118be7f66cee78aca71b3
AaronConlon <rivenqinyy@gmail.com>
2026-07-09 19:08:06 +0800

    fix: add rss link

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,5 +1,6 @@
 import { getPublishedIssues } from "@/features/blog-data";
 
+import { CONFIG } from "@/config";
 import Link from "next/link";
 import BlogItem from "./Blog/Item";
 import RoughNotationText from "./RoughNotationText";
@@ -14,9 +15,11 @@ export default async function Blogs() {
       <p className="max-w-[760px] text-center mx-auto">
         工作和学习中遇到问题时，我会把一些经验记录下来，方便之后复盘。
         如果你感兴趣，也可以
-        <RoughNotationText type="highlight" color="#fff176">
-          订阅我的博客
-        </RoughNotationText>
+        <Link target="_blank" href={CONFIG.rss} type="application/rss+xml">
+          <RoughNotationText type="highlight" color="#fff176">
+            订阅我的博客
+          </RoughNotationText>
+        </Link>
       </p>
       <div className="max-w-[1440px] mx-auto p-4 px-6 xl:px-12 2xl:px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 relative">
         {issues.slice(0, 9).map((issue) => (
